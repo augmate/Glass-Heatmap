bd407083cb5f83c1d0b1b37f4358c58aaf149013
Johny Urgiles <johny@augmate.com>
2014-07-21 22:24:24 -0400

    reading data and rendering basic cubes without lighting

---
+++ unity/Assets/readFile.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+import System.IO;
+
+function Start () {
+	var sr = new StreamReader("../data/sample_data.txt");
+	var line = sr.ReadLine();
+	
+	while(line != null){
+		print(line);		
+		var pos = line.Split(" "[0]);
+		var cube : GameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		
+		cube.transform.position = Vector3(int.Parse(pos[0]),
+									      int.Parse(pos[1]),
+									      int.Parse(pos[2]));
+			
+		cube.transform.localScale = Vector3(2, 2, 2);		
+		line = sr.ReadLine();
+	}
+	
+	sr.Close();
+}
+
+function Update () {
+}
